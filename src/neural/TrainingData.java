df91ecad0bfb9ad3b4f950a6b62204c4c096213a
Julian Møller <julian@julianmoeller.dk>
2013-10-14 10:44:35 +0200

    Add Neural Network code

---
+++ src/neural/TrainingData.java
@@ -0,0 +1,11 @@
+package neural;
+
+public class TrainingData {
+	public double[] inputs;
+	public double[] expectedOutputs;
+	
+	public TrainingData(double[] inputs, double[] expectedOutputs) {
+		this.inputs = inputs;
+		this.expectedOutputs = expectedOutputs;
+	}
+}
